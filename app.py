a83fb7376c0b6022b13930cc1ee428298e82b53b
Kelvin Mhacwilson <Kelvinlinnaeus@gmail.com>
2025-02-06 16:36:04 +0000

    Updated User Interface

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
 import numpy as np
 import pandas as pd
@@ -18,47 +18,50 @@ def home():
 
 @app.route("/predict-house-price", methods=["POST"])
 def predict():
-    if request.form['country'] != 'USA':
-        return render_template("index.html", prediction="This model is only trained on USA data. Please enter a USA address.")
-
-    input_data = {
-        'bedrooms': float(request.form["bedrooms"]),
-        'bathrooms': float(request.form["bathrooms"]),
-        'sqft_living': float(request.form["sqft_living"]),
-        'sqft_lot': float(request.form["sqft_lot"]),
-        'floors': float(request.form["floors"]),
-        'waterfront': float(request.form["waterfront"]),
-        'view': float(request.form["view"]),
-        'condition': float(request.form["condition"]),
-        'sqft_above': float(request.form["sqft_above"]),
-        'sqft_basement': float(request.form["sqft_basement"]),
-        'yr_built': float(request.form["yr_built"]),
-        'yr_renovated': float(request.form["yr_renovated"]),
-        'city': request.form["city"],
-        'statezip': request.form["statezip"],
-        'street': request.form["street"],
-        'country': request.form["country"],
-        'date': request.form["date"]
-    }
-
-    input_df = pd.DataFrame([input_data])
-    input_df = input_df.drop(['country', 'street', 'date'], axis=1)
-
     try:
-        input_df['city'] = encoder.transform(input_df[['city']])
-    except ValueError:
-        input_df['city'] = encoder.transform([['unknown']])[0]
+        if request.form['country'] != 'USA':
+            return jsonify({'error': "This model is only trained on USA data. Please change the country to USA."})
 
-    try:
-        input_df['statezip']= input_df['statezip'].str.extract(r'(\d+)').astype(int)
-    except ValueError:
-        input_df['statezip'] = encoder.transform([['unknown']])[0]
+        input_data = {
+            'bedrooms': float(request.form["bedrooms"]),
+            'bathrooms': float(request.form["bathrooms"]),
+            'sqft_living': float(request.form["sqft_living"]),
+            'sqft_lot': float(request.form["sqft_lot"]),
+            'floors': float(request.form["floors"]),
+            'waterfront': float(request.form["waterfront"]),
+            'view': float(request.form["view"]),
+            'condition': float(request.form["condition"]),
+            'sqft_above': float(request.form["sqft_above"]),
+            'sqft_basement': float(request.form["sqft_basement"]),
+            'yr_built': float(request.form["yr_built"]),
+            'yr_renovated': float(request.form["yr_renovated"]),
+            'city': request.form["city"],
+            'statezip': request.form["statezip"],
+            'street': request.form["street"],
+            'country': request.form["country"],
+            'date': request.form["date"]
+        }
 
-    input_df[input_cols] = scaler.transform(input_df[input_cols])
+        input_df = pd.DataFrame([input_data])
+        input_df = input_df.drop(['country', 'street', 'date'], axis=1)
 
-    prediction = model.predict(input_df)[0]
+        try:
+            input_df['city'] = encoder.transform(input_df[['city']])
+        except ValueError:
+            return jsonify({'error': "Invalid city. Please enter a valid city."})
 
-    return render_template("index.html", prediction=f"{prediction:,.2f}")
+        try:
+            input_df['statezip'] = input_df['statezip'].str.extract(r'(\d+)').astype(int)
+        except ValueError:
+            return jsonify({'error': "Invalid statezip. Please enter a valid statezip."})
+
+        input_df[input_cols] = scaler.transform(input_df[input_cols])
+
+        prediction = model.predict(input_df)[0]
+
+        return jsonify({'prediction': f"{prediction:,.2f}"})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == "__main__":
     app.run(debug=True)
